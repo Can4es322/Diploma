89b3f456e1d7ea4cb71175abd0e4cad4efe2f879
Can4es322 <kurys.08@gmail.com>
2023-03-14 17:33:47 +0300

    Init

---
+++ Diploma/DiplomaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiplomaApp.swift
+//  Diploma
+//
+//  Created by Dunice on 14.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct DiplomaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
